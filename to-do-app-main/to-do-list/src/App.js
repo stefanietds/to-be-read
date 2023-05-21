951431d2ee49b455a9d37288079577b3f04c62ba
stefanietds <stefanietavares10@hotmail.com>
2023-05-21 16:40:47 -0300

    filtrar livros

--- to-do-app-main/to-do-list/src/App.js
+++ to-do-app-main/to-do-list/src/App.js
@@ -7,6 +7,14 @@ function App() {
 	const [itens, setItens] = useState([]);
 	const [filter, setFilter] = useState({ filter: false, active: false })
 
+	//me
+	const [search, setSearch] = React.useState("");
+	console.log(search);
+	const searchLowerCase = search.toLowerCase()
+	
+	//end
+	
+
 	function getList() {
 		fetch('http://localhost:3000/to-do/list')
 			.then(response => response.json())
@@ -44,11 +52,16 @@ function App() {
 		getList()
 	}, [])
 
+	//me code
+	const items = itensToShow.filter((item) => 
+	item.text.toLowerCase().includes(searchLowerCase));
+	//end
+
 	return (
 
 		<div className="main">
 			<div className="to-do-list">
-				<h1>To Do List</h1>
+				<h1>To Be Read</h1>
 
 				{itensToShow.map((item) => {
 					return (
@@ -63,12 +76,27 @@ function App() {
 				<div className="rowContainer">
 					<span className={"filter"} style={filter.filter ? {} : { fontWeight: "bold" }} onClick={() => setFilter({ filter: false })}>Todos</span>
 					<span className={"filter"} style={(filter.filter && filter.active === true) ? { fontWeight: "bold" } : {}} onClick={() => setFilter({ filter: true, active: true })}>Pendentes</span>
-					<span className={"filter"} style={(filter.filter && filter.active === false) ? { fontWeight: "bold" } : {}} onClick={() => setFilter({ filter: true, active: false })}> Concluídos</span>
+					<span className={"filter"} style={(filter.filter && filter.active === false) ? { fontWeight: "bold" } : {}} onClick={() => setFilter({ filter: true, active: false })}>Concluidos</span>
 				</div>
 				<div className="rowContainer">
-					<button onClick={handleAdd}>Adicionar To Do</button>
+					<button onClick={handleAdd}>Adicionar</button>
 				</div>
+
+				
+			   <h3>Filtrar To be Read</h3>
+				<input type="search" className="card" value={search} 
+				onChange={(e) => setSearch(e.target.value)}/>
+				<ul>
+					{items.map((item) => (
+					<li key={item.text} className="card" style={{width:'full'}}>
+						<p>{item.text}</p>
+					</li>
+					))}
+				</ul>
+				
+
 			</div>
+
 		</div>
 	);
 }
